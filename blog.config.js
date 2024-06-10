108be9120e3c57f0acc1cc2b0d5564f9cd281345
miao114514awa <162010926+miao114514awa@users.noreply.github.com>
2024-06-10 14:11:44 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -321,6 +321,13 @@ const BLOG = {
       url: 'https://music.163.com/song/media/outer/url?id=731355.mp3',
       cover:
         'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+    },
+    {
+      name: 'All My People',
+      artist: 'Falcom Sound Team jdk',
+      url: 'https://music.163.com/song/media/outer/url?id=28551589.mp3',
+      cover:
+        'https://p2.music.126.net/hzsfI8q3RZsWecR1gCEKgg==/5998935441412908.jpg?param=130y130'
     }
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
